f2d0a551b106ff949ceb2f6749df10f940d876ee
Guru-Sandhu <arsh3874@gmail.com>
2020-03-09 00:21:20 -0700

    working Solution

--- app.js
+++ app.js
@@ -5,38 +5,38 @@ const fs = require('fs')
 const server = http.createServer((req, res) => {
   const {pathname, query} = url.parse(req.url,true);
   let body = '';
-  req.on('data', (chunk) =>{
-    body += chunk;
-    console.log(body);
-  })
-  req.on('end', () =>{
-    res.writeHead(200, {'content-type': 'text/html'});
-    res.write(`
-      <main>
-        <h1>Welcome to Dictionary</h1>
-        <form>
-          <label for='word'>Word</label>
-          <input name='word'>
-        </form>
-      </main>
-    `)
-    fs.readFile('./data.txt','utf8',(err,foundWord) => {
-      if (err) {
-        console.log(err)
+  let foundWord = [];
+  fs.readFile('./data.txt','utf8',(err,fileData) => {
+    if (err) {
+      console.log(err)
+    }
+    let arrOfWords = fileData.split('\n')
+    
+    for (let i = 0;i < arrOfWords.length; i++) {
+      if (arrOfWords[i].startsWith(query.word)) {
+        foundWord.push(arrOfWords[i]);
       }
-      let arrOfWords = foundWord.split('\n')
-      
-      for (let char of arrOfWords) {
-        if (char.startsWith(query.word)) {
-          res.write(`
-            <h1>Defination</h1>
-            <p>${char}<p>
-          `)
-          console.log(char);
-        }
-      }
+    }
+    console.log(foundWord)
+    req.on('data', (chunk) =>{
+      body += chunk;
+      console.log(body);
     })
-    res.end()
+    req.on('end', () =>{
+      res.writeHead(200, {'content-type': 'text/html'});
+      res.write(`
+        <main>
+          <h1>Welcome to Dictionary</h1>
+          <form>
+            <label for='word'>Word</label>
+            <input name='word'>
+          </form>
+          <h2>Defination:</h2>
+          <p>${foundWord[0]}</p>
+        </main>
+      `)
+      res.end()
+    })
   })
 });
 
